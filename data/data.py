8bb334d7a368ae9429b6398f8dd800964d10fcd0
newbee <beenewminh@outlook.com>
2023-09-23 05:55:11 +1000

    feat: add server

--- data/data.py
+++ data/data.py
@@ -17,70 +17,72 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 # Cause we only have one data file
 from sklearn.model_selection import train_test_split
 
+import sys
+sys.path.append("..")
+
+from utils import scaler, rescaler
+
 
 # # Functions
 
 # In[ ]:
 
-
 def process_data(data, lags):
-    """
-    Process Data
-    Reshape and split data into train and test data.
-    
-    Parameters:
-        data (str): name of the excel data file
-        lags (int): time lag
-    
-    Returns:
-        X_train (np.ndarray)
-        y_train (np.ndarray)
-        X_test (np.ndarray)
-        y_test (np.ndarray)
-        flow_scaler (func)
-        flow_rescaler (func)
-        latlong_scaler (MinMaxScaler)
-    """
-    df = pd.read_excel(data, sheet_name="Data", header=[1])
+	"""
+	Process Data
+	Reshape and split data into train and test data.
+	
+	Parameters:
+		data (str): name of the excel data file
+		lags (int): time lag
+	
+	Returns:
+		X_train (np.ndarray)
+		y_train (np.ndarray)
+		X_test (np.ndarray)
+		y_test (np.ndarray)
+		flow_scaler (func)
+		flow_rescaler (func)
+		latlong_scaler (MinMaxScaler)
+	"""
+	df = pd.read_excel(data, sheet_name="Data", header=[1])
 
-    first_velo_col_pos = df.columns.get_loc("V00")
-    flow_group = np.char.mod("V%02d", np.arange(0, 96))
-    grouped = df.groupby(['NB_LATITUDE', 'NB_LONGITUDE'])[flow_group].apply(lambda x: x.values.tolist())
+	first_velo_col_pos = df.columns.get_loc("V00")
+	flow_group = np.char.mod("V%02d", np.arange(0, 96))
+	grouped = df.groupby(['NB_LATITUDE', 'NB_LONGITUDE'])[flow_group].apply(lambda x: x.values.tolist())
 
-    flow_data = grouped.values
-    flow_data = grouped.values
-    flow_max = np.array(flow_data.max()).max()
-    flow_min = np.array(flow_data.min()).min()
-    def flow_scaler(x):
-        return (x - flow_min) / (flow_max - flow_min)
+	flow_data = grouped.values
+	flow_data = grouped.values
+	flow_max = np.array(flow_data.max()).max()
+	flow_min = np.array(flow_data.min()).min()
 
-    def flow_rescaler(x):
-        return x * (flow_max - flow_min) + flow_min
+	flow_scaler = scaler(flow_min, flow_max)
+	flow_rescaler = rescaler(flow_min, flow_max)
 
-    latlong_data = np.array(grouped.index.to_list())
-    latlong_scaler = MinMaxScaler(feature_range=(0, 1)).fit(latlong_data.reshape(-1, 1))
-    latlong_data = latlong_scaler.transform(latlong_data.reshape(-1, 1)).reshape(-1, 2)
+	latlong_data = np.array(grouped.index.to_list())
+	latlong_scaler = MinMaxScaler(feature_range=(0, 1)).fit(latlong_data.reshape(-1, 1))
+	latlong_data = latlong_scaler.transform(latlong_data.reshape(-1, 1)).reshape(-1, 2)
 
-    train = []
+	train = []
 
-    i = 0
-    for flow in grouped.values:
-        flow = np.array(flow, dtype=float).flatten()
-        flow = np.vectorize(flow_scaler)(flow)
-        indices = np.arange(lags, len(flow))
-        offset = np.arange(-lags, 1)
-        flow = flow[indices[:, np.newaxis] + offset]
-        latlong = np.tile(latlong_data[i], (len(flow), 1))
-        combined_arr = np.hstack((latlong, flow))
-        train.extend(combined_arr)
-        i += 1
+	i = 0
+	for flow in grouped.values:
+		flow = np.array(flow, dtype=float).flatten()
+		flow = np.vectorize(flow_scaler)(flow)
+		indices = np.arange(lags, len(flow))
+		offset = np.arange(-lags, 1)
+		flow = flow[indices[:, np.newaxis] + offset]
+		latlong = np.tile(latlong_data[i], (len(flow), 1))
+		combined_arr = np.hstack((latlong, flow))
+		train.extend(combined_arr)
+		i += 1
 
-    train = np.array(train)
+	train = np.array(train)
 
-    X = train[:, :-1]
-    y = train[:, -1]
+	X = train[:, :-1]
+	y = train[:, -1]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, train_size = .75)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, train_size = .75)
 
-    return X_train, y_train, X_test, y_test, flow_scaler, flow_rescaler, latlong_scaler
+	return X_train, y_train, X_test, y_test, flow_scaler, flow_rescaler, latlong_scaler
 
--- main.py
+++ main.py
@@ -18,137 +18,138 @@ warnings.filterwarnings("ignore")
 
 
 def MAPE(y_true, y_pred):
-    """Mean Absolute Percentage Error
-    Calculate the mape.
+	"""Mean Absolute Percentage Error
+	Calculate the mape.
 
-    # Arguments
-        y_true: List/ndarray, ture data.
-        y_pred: List/ndarray, predicted data.
-    # Returns
-        mape: Double, result data for train.
-    """
+	# Arguments
+		y_true: List/ndarray, ture data.
+		y_pred: List/ndarray, predicted data.
+	# Returns
+		mape: Double, result data for train.
+	"""
 
-    y = [x for x in y_true if x > 0]
-    y_pred = [y_pred[i] for i in range(len(y_true)) if y_true[i] > 0]
+	y = [x for x in y_true if x > 0]
+	y_pred = [y_pred[i] for i in range(len(y_true)) if y_true[i] > 0]
 
-    num = len(y_pred)
-    sums = 0
+	num = len(y_pred)
+	sums = 0
 
-    for i in range(num):
-        tmp = abs(y[i] - y_pred[i]) / y[i]
-        sums += tmp
+	for i in range(num):
+		tmp = abs(y[i] - y_pred[i]) / y[i]
+		sums += tmp
 
-    mape = sums * (100 / num)
+	mape = sums * (100 / num)
 
-    return mape
+	return mape
 
 
 def eva_regress(y_true, y_pred):
-    """Evaluation
-    evaluate the predicted resul.
+	"""Evaluation
+	evaluate the predicted resul.
 
-    # Arguments
-        y_true: List/ndarray, ture data.
-        y_pred: List/ndarray, predicted data.
-    """
+	# Arguments
+		y_true: List/ndarray, ture data.
+		y_pred: List/ndarray, predicted data.
+	"""
 
-    mape = MAPE(y_true, y_pred)
-    vs = metrics.explained_variance_score(y_true, y_pred)
-    mae = metrics.mean_absolute_error(y_true, y_pred)
-    mse = metrics.mean_squared_error(y_true, y_pred)
-    r2 = metrics.r2_score(y_true, y_pred)
-    print('explained_variance_score:%f' % vs)
-    print('mape:%f%%' % mape)
-    print('mae:%f' % mae)
-    print('mse:%f' % mse)
-    print('rmse:%f' % math.sqrt(mse))
-    print('r2:%f' % r2)
+	mape = MAPE(y_true, y_pred)
+	vs = metrics.explained_variance_score(y_true, y_pred)
+	mae = metrics.mean_absolute_error(y_true, y_pred)
+	mse = metrics.mean_squared_error(y_true, y_pred)
+	r2 = metrics.r2_score(y_true, y_pred)
+	print('explained_variance_score:%f' % vs)
+	print('mape:%f%%' % mape)
+	print('mae:%f' % mae)
+	print('mse:%f' % mse)
+	print('rmse:%f' % math.sqrt(mse))
+	print('r2:%f' % r2)
 
 
 def plot_results(y_true, y_preds, names, periods):
-    """Plot
-    Plot the true data and predicted data.
+	"""Plot
+	Plot the true data and predicted data.
 
-    # Arguments
-        y_true: List/ndarray, ture data.
-        y_pred: List/ndarray, predicted data.
-        names: List, Method names.
-        periods: amout of time in the plots
-    """
-    d = '2006-10-01 00:00'
-    y_true = y_true[:periods]
-    x = pd.date_range(d, periods=periods, freq='15min')
+	# Arguments
+		y_true: List/ndarray, ture data.
+		y_pred: List/ndarray, predicted data.
+		names: List, Method names.
+		periods: amout of time in the plots
+	"""
+	d = '2006-10-01 00:00'
+	y_true = y_true[:periods]
+	x = pd.date_range(d, periods=periods, freq='15min')
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
 
-    ax.plot(x, y_true, label='True Data')
-    for name, y_pred in zip(names, y_preds):
-        ax.plot(x, y_pred, label=name)
+	ax.plot(x, y_true, label='True Data')
+	for name, y_pred in zip(names, y_preds):
+		ax.plot(x, y_pred, label=name)
 
-    plt.legend()
-    plt.grid(True)
-    plt.xlabel('Time of Day')
-    plt.ylabel('Flow')
+	plt.legend()
+	plt.grid(True)
+	plt.xlabel('Time of Day')
+	plt.ylabel('Flow')
 
-    date_format = mpl.dates.DateFormatter("%H:%M")
-    ax.xaxis.set_major_formatter(date_format)
-    fig.autofmt_xdate()
+	date_format = mpl.dates.DateFormatter("%H:%M")
+	ax.xaxis.set_major_formatter(date_format)
+	fig.autofmt_xdate()
 
 
-    now = datetime.now()
-    current_time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    plt.savefig(f'output-{current_time}.png', dpi=2400)
+	now = datetime.now()
+	current_time = now.strftime("%m-%d-%Y-%H-%M-%S")
+	plt.savefig(f'output-{current_time}.png', dpi=2400)
 
-    plt.show()
+	plt.show()
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--days",
-        default=1,
-        help="Number of days in the plots.")
-    parser.add_argument(
-        "--hours",
-        action='store_true',
-        default=24,
-        help="Number of hours per day in the plots.")
-    parser.add_argument(
-        "--lags",
-        type=int,
-        default=7,
-        help="Lags in the model.\nPlease use the same one you use for train.py")
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"--days",
+		default=1,
+		help="Number of days in the plots.")
+	parser.add_argument(
+		"--hours",
+		action='store_true',
+		default=24,
+		help="Number of hours per day in the plots.")
+	parser.add_argument(
+		"--lags",
+		type=int,
+		default=7,
+		help="Lags in the model.\nPlease use the same one you use for train.py")
+	args = parser.parse_args()
 
-    lstm = load_model('model/lstm.h5')
-    gru = load_model('model/gru.h5')
-    saes = load_model('model/saes.h5')
-    models = [lstm, gru, saes]
-    names = ['LSTM', 'GRU', 'SAEs']
+	lstm = load_model('model/lstm.h5')
+	gru = load_model('model/gru.h5')
+	saes = load_model('model/saes.h5')
+	models = [lstm, gru, saes]
+	names = ['LSTM', 'GRU', 'SAEs']
 
-    file = 'data/Scats Data October 2006.xls'
-    _, _, X_test, y_test, _, flow_rescaler, _ = process_data(file, args.lags)
-    y_test = np.vectorize(flow_rescaler)(y_test)
+	file = 'data/Scats Data October 2006.xls'
+	_, _, X_test, y_test, _, flow_rescaler, _ = process_data(file, args.lags)
+	y_test = np.vectorize(flow_rescaler)(y_test)
 
-    periods = args.days * args.hours * 60 // 15
+	periods = args.days * args.hours * 60 // 15
 
-    y_preds = []
-    for name, model in zip(names, models):
-        if name == 'SAEs':
-            X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1]))
-        else:
-            X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-        file = 'images/' + name + '.png'
-        plot_model(model, to_file=file, show_shapes=True)
-        predicted = model.predict(X_test)
-        predicted = np.vectorize(flow_rescaler)(predicted)
-        y_preds.append(predicted[:periods])
-        print(name)
-        eva_regress(y_test, predicted)
+	y_preds = []
+	for name, model in zip(names, models):
+		print(X_test)
+		if name == 'SAEs':
+			X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1]))
+		else:
+			X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+		file = 'images/' + name + '.png'
+		plot_model(model, to_file=file, show_shapes=True)
+		predicted = model.predict(X_test)
+		predicted = np.vectorize(flow_rescaler)(predicted)
+		y_preds.append(predicted[:periods])
+		print(name)
+		eva_regress(y_test, predicted)
 
-    plot_results(y_test[:periods], y_preds, names, periods)
+	plot_results(y_test[:periods], y_preds, names, periods)
 
 
 if __name__ == '__main__':
-    main()
+	main()
