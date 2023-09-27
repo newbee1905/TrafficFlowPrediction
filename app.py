fix: re-add latlong scalling + fix scaling in api

app.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 import pandas as pd
 import numpy as np
 from keras.models import load_model
+from data.data import read_excel_data, process_data
 
 from utils import scaler, rescaler
 
@@ -12,7 +13,8 @@ import uvicorn
 app = FastAPI()
 
 file = 'data/Scats Data October 2006.xls'
-df = pd.read_excel(file, sheet_name="Data", header=[1])
+df = read_excel_data(file)
+_, _, _, _, flow_scaler, flow_rescaler, latlong_scaler = process_data(df, 7)
 
 first_velo_col_pos = df.columns.get_loc("V00")
 flow_group = np.char.mod("V%02d", np.arange(0, 96))
@@ -21,61 +23,57 @@ grouped = df.groupby(['NB_LATITUDE', 'NB_LONGITUDE'])[flow_group].apply(lambda x
 lats = grouped.index.get_level_values('NB_LATITUDE').values
 lngs = grouped.index.get_level_values('NB_LONGITUDE').values
 
+
 def find_closest_value(lat, lng):
-	# calculating the distance of all lat lng compare to the
-	# given one
-	distances = np.sqrt((lats - lat) ** 2 + (lngs - lng) ** 2)
+    # calculating the distance of all lat lng compare to the
+    # given one
+    distances = np.sqrt((lats - lat) ** 2 + (lngs - lng) ** 2)
 
-	# get index of the one closest to 0
-	closest_index = np.argmin(distances)
+    # get index of the one closest to 0
+    closest_index = np.argmin(distances)
 
-	return grouped.index[closest_index]
+    return grouped.index[closest_index]
 
 def gen_fake_flow(flows, start_time):
-	hour, minute = start_time.split(':')
-	time_pos = int(hour) * 4 + int(int(minute) / 15) + 7
-	diff = np.floor(np.random.rand(7) * 10 - 5).astype(int)
-	flows = np.append(flows[-7:], flows)
-	return flows[time_pos - 7:time_pos] + diff
+    hour, minute = start_time.split(':')
+    time_pos = int(hour) * 4 + int(int(minute) / 15) + 7
+    diff = np.floor(np.random.rand(7) * 10 - 5).astype(int)
+    flows = np.append(flows[-7:], flows)
+    return flows[time_pos - 7:time_pos] + diff
 
 class ModelEnum(str, Enum):
-	lstm = "lstm"
-	gru = "gru"
-	saes = "saes"
+    lstm = "lstm"
+    gru = "gru"
+    saes = "saes"
 
 class PredTrafficFlow(BaseModel):
-	start_time: str
-	lat: float
-	lng: float
-	model: ModelEnum
+    start_time: str
+    lat: float
+    lng: float
+    model: ModelEnum
 
 
 models = {
-	"lstm": load_model('model/lstm.h5'),
-	"gru": load_model('model/gru.h5'),
-	"saes": load_model('model/saes.h5'),
+    "lstm": load_model('model/lstm.h5'),
+    "gru": load_model('model/gru.h5'),
+    "saes": load_model('model/saes.h5'),
 }
 
 @app.post("/")
 def root(pred: PredTrafficFlow) -> float:
-	lat, lng = find_closest_value(pred.lat, pred.lng)
-	flows = gen_fake_flow(grouped[(lat, lng)], pred.start_time)
+    lat, lng = find_closest_value(pred.lat, pred.lng)
+    latlong = np.array([lat, lng])
+    latlong = latlong_scaler.transform(latlong.reshape(-1, 1)).reshape(-1, 2)
+    flows = gen_fake_flow(grouped[(lat, lng)], pred.start_time)
 
-	flow_min = flows.min()
-	flow_max = flows.max()
+    X = np.array([np.append(latlong, np.vectorize(flow_scaler)(flows))])
 
-	flow_scaler = scaler(flow_min, flow_max)
-	flow_rescaler = rescaler(flow_min, flow_max)
+    if pred.model == 'saes':
+        X = np.reshape(X, (X.shape[0], X.shape[1]))
+    else:
+        X = np.reshape(X, (X.shape[0], X.shape[1], 1))
 
-	X = np.array([np.append([lat, lng], np.vectorize(flow_rescaler)(flows))])
+    predict = models[pred.model].predict(X)
+    predict = np.vectorize(flow_rescaler)(predict)
 
-	if pred.model == 'saes':
-		X = np.reshape(X, (X.shape[0], X.shape[1]))
-	else:
-		X = np.reshape(X, (X.shape[0], X.shape[1], 1))
-
-	predict = models[pred.model].predict(X)
-
-	predict = np.vectorize(flow_rescaler)(predict)
-
-	return predict[0][0]
+    return predict[0][0]

data/data.py
@@ -91,7 +91,7 @@ def process_data(df: pd.DataFrame, lags: int) -> tuple[np.ndarray, np.ndarray, n
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, train_size=.75)
 
-    return X_train, y_train, X_test, y_test, flow_scaler, flow_rescaler
+    return X_train, y_train, X_test, y_test, flow_scaler, flow_rescaler, latlong_scaler
 
 def process_data_prophet(df: pd.DataFrame):
     first_velo_col_pos = df.columns.get_loc("V00")

main.py
@@ -133,7 +133,7 @@ def main():
 
     file = 'data/Scats Data October 2006.xls'
     df = read_excel_data(file)
-    _, _, X_test, y_test, _, flow_rescaler = process_data(df, args.lags)
+    _, _, X_test, y_test, _, flow_rescaler, _ = process_data(df, args.lags)
     y_test = np.vectorize(flow_rescaler)(y_test)
     _, prophet_test = process_data_prophet(df)
 