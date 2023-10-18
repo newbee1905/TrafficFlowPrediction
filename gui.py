4cddd658842dab7c927f161eae44c8097655adce
erikfanning <132543883+erikfanning@users.noreply.github.com>
2023-10-18 14:32:12 +1100

    Added volume prediction option in GUI

--- gui.py
+++ gui.py
@@ -2,11 +2,13 @@ import tkinter as tk
 from tkinter import ttk
 import networkx as nx
 import pandas as pd
-from process import calculate_time_with_flow, haversine, find_route, find_closest_nodes_by_scats, visualize_route_on_map, G as G
+from process import get_traffic_flow, find_route, find_closest_nodes_by_scats, visualize_route_on_map, G as G
 import webbrowser
 
 # Load the SCATS data into a DataFrame
 scats_data = pd.read_excel('Scats Data October 2006.xls', sheet_name='Data', skiprows=1)
+
+time_options = ["0:00", "0:15", "0:30", "0:45", "1:00", "1:15", "1:30", "1:45", "2:00", "2:15", "2:30", "2:45", "3:00", "3:15", "3:30", "3:45", "4:00", "4:15", "4:30", "4:45", "5:00", "5:15", "5:30", "5:45", "6:00", "6:15", "6:30", "6:45", "7:00", "7:15", "7:30", "7:45", "8:00", "8:15", "8:30", "8:45", "9:00", "9:15", "9:30", "9:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45"]
 
 def open_map_in_browser():
     webbrowser.open("route_map.html", new=2)
@@ -45,6 +47,36 @@ def calculate_route():
     else:
         result_label.config(text="Please enter valid SCATS numbers for both start and end intersections.")
 
+def get_intersection_by_scats(scats_number, scats_data, G):
+    # Use the existing function to find the closest nodes
+    closest_node, _ = find_closest_nodes_by_scats(scats_number, scats_number, scats_data, G)
+    # The closest_node is the intersection name you're looking for
+    return closest_node
+
+def get_flow_data():
+    # Get the entered SCATS number as an integer
+    try:
+        scats_number = int(scats_var.get())
+    except ValueError:
+        flow_data_label.config(text="Please enter a valid integer for SCATS number.")
+        return
+        # Get the intersection name by the SCATS number
+    intersection_name = get_intersection_by_scats(scats_number, scats_data, G)
+
+    if intersection_name is None:
+        flow_data_label.config(text="No intersection found for the provided SCATS number.")
+        return
+
+    # Get the selected time and model
+    selected_time = flow_time_dropdown.get()
+    selected_model = flow_model_dropdown.get()
+
+    # Call the get_traffic_flow method
+    flow_data = get_traffic_flow(selected_time, intersection_name, selected_model)
+
+    # Display the flow data in the GUI
+    flow_data_label.config(text=f"Traffic Volume for SCATS site: {scats_number}\n{intersection_name}\nAt {selected_time} using {selected_model}:\n{flow_data}")
+
 
 root = tk.Tk()
 root.title("Route Finder")
@@ -53,6 +85,34 @@ frm = ttk.Frame(root, padding=10)
 frm.grid(row=0, column=0, sticky=(tk.N, tk.W, tk.E, tk.S))
 frm.columnconfigure(0, minsize=20)  
 frm.columnconfigure(1, minsize=20)  
+
+# Entry field for scats site for volume prediction
+ttk.Label(frm, text="Enter SCATS Number for Traffic Volume:").grid(column=0, row=7, sticky=tk.W)
+scats_var = tk.StringVar()
+scats_entry = ttk.Entry(frm, textvariable=scats_var, width=20)
+scats_entry.grid(column=1, row=7, sticky=(tk.W, tk.E))
+
+# Dropdown for time selection specific to flow data
+ttk.Label(frm, text="Select Time for Volume Prediction:").grid(column=0, row=8, sticky=tk.W)
+flow_time_dropdown = ttk.Combobox(frm, values=time_options, width=20) 
+flow_time_dropdown.grid(column=1, row=8, sticky=tk.W)
+flow_time_dropdown.set("0:00") 
+
+# Dropdown for ML model selection specific to flow data
+ttk.Label(frm, text="Select Model for Predicting Traffic Volume:").grid(column=0, row=9, sticky=tk.W)
+flow_model_options = ["LSTM", "GRU", "SAES"]
+flow_model_var = tk.StringVar()
+flow_model_dropdown = ttk.Combobox(frm, values=flow_model_options, textvariable=flow_model_var, width=20) 
+flow_model_dropdown.grid(column=1, row=9, sticky=tk.W)
+flow_model_dropdown.set("LSTM") 
+
+# Button to get flow data
+get_flow_button = ttk.Button(frm, text="Predict Volume", command=get_flow_data)
+get_flow_button.grid(column=0, row=10, columnspan=2)
+
+# Label to display the flow data
+flow_data_label = ttk.Label(frm, text="")
+flow_data_label.grid(column=0, row=21, columnspan=2, sticky=(tk.W, tk.E))
 
 ttk.Label(frm, text="Enter Start SCATS Number:").grid(column=0, row=0, sticky=tk.W)
 
